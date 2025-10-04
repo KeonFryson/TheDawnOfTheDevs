df513a5a42ddbf4d20338404ce29dd136a8a109e
Keon Fryson <frysonk112115@gmail.com>
2025-10-04 15:25:18 -0400

    Import UnityTechnologies Final assets and scripts
    Added a large set of assets, prefabs, scenes, scripts, and supporting files from UnityTechnologies' Final package. This includes environment art, animations, controllers, UI elements, prefabs for characters and effects, sample scenes, and core gameplay scripts for DamageZone, Enemy, HealthCollectible, NonPlayerCharacter, Projectile, RubyController, and UIHandler.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //public InputAction LeftAction;
+    public InputAction MoveAction;
+    void Start()
+    {
+        //LeftAction.Enable(); 
+        MoveAction.Enable();
+    }
+
+    void Update()
+    {
+ 
+  
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.01f;
+        transform.position = position;
+     
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/////////////////Debugging START//////////////////////
+
+
+
+
+
+/////////////////Debugging END//////////////////////
+
+
+
+
+
+
+
+
+
+/////////////////Removed START/////////////////////
+//void Update()
+
+//{
+//float horizontal = 0.0f;
+//float vertical = 0.0f;
+
+////Left key
+//if (LeftAction.IsPressed())
+//{
+//    horizontal = -1.0f;
+//}
+////Right key
+//else if (Keyboard.current.rightArrowKey.isPressed)
+//{
+//    horizontal = 1.0f;
+//}
+////Up key
+//if (Keyboard.current.upArrowKey.isPressed)
+//{
+//    vertical = 1.0f;
+//}
+////Down key
+//else if (Keyboard.current.downArrowKey.isPressed)
+//{
+//    vertical = -1.0f;
+
+//}
+
+//Debug.Log(horizontal);
+//Debug.Log(vertical);
+
+
+//Vector2 position = transform.position;
+
+//position.x = position.x + 0.1f * horizontal;
+//position.y = position.y + 0.1f * vertical;
+//transform.position = position;
+//}
+//////////////Removed END
